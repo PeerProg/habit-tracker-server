487bd1d6a25a2bdec9f54958229a753a3ef6e523
oreoluwade <oreoluwade@gmail.com>
2018-04-27 14:34:31 +0100

    create home and user route, mount on app

---
+++ server/routes/user.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import userController from '../controllers';
+
+const router = express.Router();
+
+router.route('/')
+  .post(userController.createUser);
+
+export default router;
