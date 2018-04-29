b9ff69f0fd07f4c5c5dc756d9f96b69f47972fc0
oreoluwade <oreoluwade@gmail.com>
2018-04-29 05:56:22 +0100

    create user and authentication test suites

---
+++ tests/authentication.test.js
@@ -0,0 +1,55 @@
+import request from 'supertest';
+import app from '../app';
+import models from '../server/models';
+
+const server = request(app);
+
+describe('Authentication Test Suite', () => {
+  let createdToken;
+  beforeAll((done) => {
+    models.sequelize.sync({ force: true })
+      .then(() => {
+        const newUserBody = {
+          username: 'newuser',
+          email: 'newuser@user.com',
+          password: 'administrator'
+        };
+
+        server.post('/user/register')
+          .send(newUserBody)
+          .end((err, res) => {
+            if (err) return err;
+            createdToken = res.body.token;
+            done();
+          });
+      }).catch(err => err);
+  });
+
+  it('It should not authenticate a user with no token', (done) => {
+    server.get('/user/allusers')
+      .end((err, res) => {
+        expect(res.status).toEqual(401);
+        done();
+      });
+  });
+
+  it('should not authenticate a user with an invalid token', (done) => {
+    server.get('/user/allusers')
+      .set({ Authorization: 'trinity' })
+      .end((err, res) => {
+        expect(res.status).toEqual(401);
+        done();
+      });
+  });
+
+  it('should return all users when valid token is supplied', (done) => {
+    server.get('/user/allusers')
+      .set({ Authorization: createdToken })
+      .end((err, res) => {
+        expect(res.status).toEqual(200);
+        expect(Array.isArray(res.body.users)).toBe(true);
+        expect(res.body.totalNumberOfUsers).toEqual(1);
+        done();
+      });
+  });
+});
