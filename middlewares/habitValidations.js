055745140618cf4db4e2f5fb78a0b01751486cff
oreoluwade <oreoluwade@gmail.com>
2018-11-26 12:36:17 +0100

    add required validations for habit creation

--- middlewares/habitValidations.js
+++ middlewares/habitValidations.js
@@ -1,10 +1,45 @@
 import { Op } from 'sequelize';
 import models from '../models';
+import { isEmpty } from '../helpers';
+
 
 const { Users, Habits } = models;
 
+const expectedFields = ['name', 'milestones'];
+
 export default {
-  async hasSimilarlyNamedHabit(req, res, next) {
+  async ensureNameAndMilestonesSupplied(req, res, next) {
+    const requiredFieldsArray = [];
+
+    expectedFields.forEach(field => {
+      if (!Object.keys(req.body).includes(field)) {
+        requiredFieldsArray.push(`${field} is required but was not supplied`);
+      }
+    });
+
+    if (requiredFieldsArray.length) {
+      return res.status(403).json({ error: requiredFieldsArray });
+    }
+    return next();
+  },
+
+  async ensureNonNullFields(req, res, next) {
+    const nullFieldsArray = [];
+
+    expectedFields.forEach(field => {
+      const fieldValue = req.body[field];
+      if (isEmpty(fieldValue)) {
+        nullFieldsArray.push(`${field} should not be empty`);
+      }
+    });
+
+    if (nullFieldsArray.length) {
+      return res.status(403).json({ error: nullFieldsArray });
+    }
+    return next();
+  },
+
+  async ensureNoSimilarlyNamedHabit(req, res, next) {
     const { decoded: { id: userId }, body: { name } } = req;
     const habit = await Habits.findOne({
       where: {
--- routes/habit.js
+++ routes/habit.js
@@ -5,14 +5,24 @@ import { authentication, habitValidations } from '../middlewares';
 const router = express.Router();
 
 const { authenticateUser } = authentication;
-const { hasSimilarlyNamedHabit } = habitValidations;
+const {
+  ensureNameAndMilestonesSupplied,
+  ensureNonNullFields,
+  ensureNoSimilarlyNamedHabit
+} = habitValidations;
 const { getAllHabits, createNewHabit } = habitController;
 
 router.route('/all')
   .get(authenticateUser, getAllHabits);
 
 router.route('/create')
-  .post(authenticateUser, hasSimilarlyNamedHabit, createNewHabit);
+  .post(
+    authenticateUser,
+    ensureNameAndMilestonesSupplied,
+    ensureNonNullFields,
+    ensureNoSimilarlyNamedHabit,
+    createNewHabit
+  );
 
 
 export default router;
