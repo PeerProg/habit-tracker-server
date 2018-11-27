0092a133fb6500f37a1f1770f1576e410505b566
oreoluwade <oreoluwade@gmail.com>
2018-11-28 00:37:33 +0100

    refactor all milestone resource files

--- controllers/milestone.js
+++ controllers/milestone.js
@@ -1,4 +1,6 @@
+import { Op } from 'sequelize';
 import models from '../models';
+import { toSentenceCase } from '../helpers';
 
 const { Habits, Milestone } = models;
 
@@ -18,6 +20,92 @@ export default {
       updatedAt: milestone.updatedAt
     }));
 
-    return res.send(responseArray);
+    return res.json(responseArray);
+  },
+
+  async addMilestone(req, res) {
+    const { habitId } = req.params;
+    const title = toSentenceCase(req.body.title);
+    const newMilestone = await Milestone.create({ title, habitId });
+
+    const responseObject = {
+      id: newMilestone.id,
+      title: newMilestone.title,
+      completed: newMilestone.completed,
+      createdAt: newMilestone.createdAt,
+      updatedAt: newMilestone.updatedAt,
+    };
+
+    return res.json(responseObject);
+  },
+
+  async getMilestone(req, res) {
+    const { habitId, milestoneId } = req.params;
+    const milestone = await Milestone.findOne({
+      where: {
+        [Op.and]: [
+          {
+            id: milestoneId,
+            habitId
+          }
+        ]
+      }
+    });
+
+    const responseObject = {
+      id: milestone.id,
+      title: milestone.title,
+      completed: milestone.completed,
+      createdAt: milestone.createdAt,
+      updatedAt: milestone.updatedAt,
+    };
+
+    return res.json(responseObject);
+  },
+
+  async updateMilestone(req, res) {
+    const { habitId, milestoneId } = req.params;
+    const milestone = await Milestone.findOne({
+      where: {
+        [Op.and]: [
+          {
+            id: milestoneId,
+            habitId
+          }
+        ]
+      }
+    });
+    const title = req.body.title && toSentenceCase(req.body.title);
+    const updatedMilestone = await milestone.update({
+      title: title || milestone.title,
+      completed: req.body.completed || milestone.completed
+    });
+
+    const responseObject = {
+      id: updatedMilestone.id,
+      title: updatedMilestone.title,
+      completed: updatedMilestone.completed,
+      createdAt: updatedMilestone.createdAt,
+      updatedAt: updatedMilestone.updatedAt,
+    };
+
+    return res.json(responseObject);
+  },
+
+  async deleteMilestone(req, res) {
+    const { habitId, milestoneId } = req.params;
+    const milestone = await Milestone.findOne({
+      where: {
+        [Op.and]: [
+          {
+            id: milestoneId,
+            habitId
+          }
+        ]
+      }
+    });
+    await milestone.destroy();
+
+    return res.json({ message: 'Milestone deleted' });
   }
 };
