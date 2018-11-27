2d88d33d67b99f126f5ef3d986a2daafc542fa13
oreoluwade <oreoluwade@gmail.com>
2018-11-27 12:09:42 +0100

    remove allPurposeErrorHandler till needed

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,6 +1,5 @@
 export { default as authentication } from './authentication';
 export { default as authorization } from './authorization';
 export { default as validations } from './validations';
-export { default as routesErrorHandler } from './routesErrorHandler';
-export { default as allPurposeErrorHandler } from './allPurposeErrorHandler';
 export { default as habitValidations } from './habitValidations';
+export { default as routesErrorHandler } from './routesErrorHandler';
--- middlewares/routesErrorHandler.js
+++ middlewares/routesErrorHandler.js
@@ -1,5 +1,9 @@
+// eslint-disable-next-line no-unused-vars
 export default (req, res, next) => {
-  const error = new Error('Not Found');
-  error.status = 404;
-  next(error);
+  const error = new Error('Route not Found');
+  res.status(400).json({
+    error: {
+      message: error.message
+    }
+  });
 };
