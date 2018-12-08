567a20579da587ab9136aadd7441cb6a040196d6
KniteDev <bmose@atbtechsoft.com>
2018-12-08 01:10:02 +0100

    add imageURL migration file and add field to model

--- models/user.js
+++ models/user.js
@@ -1,50 +1,47 @@
 import bcrypt from 'bcrypt';
 
 const UserModel = (sequelize, DataTypes) => {
-  const User = sequelize.define('Users', {
-    id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      unique: true,
-      allowNull: false,
-      defaultValue: DataTypes.UUIDV4,
-    },
-    email: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false,
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    isActive: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: true,
-      allowNull: false,
-    },
-    isAdmin: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-      allowNull: false,
-    },
-    isSuperAdmin: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-      allowNull: false,
-    }
-  }, {
-    hooks: {
-      beforeCreate(user) {
-        user.hashPassword();
+  const User = sequelize.define(
+    'Users',
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        unique: true,
+        allowNull: false,
+        defaultValue: DataTypes.UUIDV4
       },
+      email: { type: DataTypes.STRING, unique: true, allowNull: false },
+      username: { type: DataTypes.STRING, allowNull: false, unique: true },
+      password: { type: DataTypes.STRING, allowNull: false },
+      isActive: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+        allowNull: false
+      },
+      isAdmin: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        allowNull: false
+      },
+      isSuperAdmin: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        allowNull: false
+      },
+      imageURL: {
+        type: DataTypes.STRING,
+        allowNull: true
+      }
     },
-  });
+    {
+      hooks: {
+        beforeCreate(user) {
+          user.hashPassword();
+        }
+      }
+    }
+  );
 
   // Instance methods
   User.prototype.hashPassword = function hashPassword() {
